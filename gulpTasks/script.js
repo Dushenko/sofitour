742ffaeb7dac0384f201972a74b7ff36c3072fee
Bully <martinborman2016@mail.ru>
2019-09-23 21:39:29 +0300

    chenge script task + animate js and scss

--- gulpTasks/script.js
+++ gulpTasks/script.js
@@ -1,14 +1,14 @@
 const include = require('gulp-include'),
-      babel = require('gulp-babel');
+    babel = require('gulp-babel');
 
 
- module.exports = function() {
-   $.gulp.task('scripts', () => {
-     return $.gulp.src('app/js/app.js')
-			 .pipe(include())
-       .pipe(babel({
-         presets: ['@babel/env']
-       }))
-       .pipe($.gulp.dest('build/js/'));
-   });
- };
+module.exports = function() {
+    $.gulp.task('scripts', () => {
+        return $.gulp.src(['app/js/app.js', 'app/js/animate.js'])
+            .pipe(include())
+            .pipe(babel({
+                presets: ['@babel/env']
+            }))
+            .pipe($.gulp.dest('build/js/'));
+    });
+};
